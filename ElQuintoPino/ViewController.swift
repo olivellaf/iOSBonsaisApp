8581cbdcdeca18e4da0afdc46d60469d0966f93c
olivellaf <ferry87@gmail.com>
2015-03-02 18:38:29 +0100

    Initial Commit

---
+++ ElQuintoPino/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ElQuintoPino
+//
+//  Created by Ferran Olivella on 2/3/15.
+//  Copyright (c) 2015 Ferran Olivella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
